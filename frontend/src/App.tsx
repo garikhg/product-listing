afd25b4b273ec20a25f9146d1dd65ee185bf783d
garikhg <garik.28h@gmail.com>
2024-09-20 22:11:27 +0400

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import {Button} from "./components/ui/button.tsx";
+
+function App() {
+
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline">
+              Hello world!
+          </h1>
+          <Button>
+              Buttons
+          </Button>
+      </>
+  )
+}
+
+export default App
